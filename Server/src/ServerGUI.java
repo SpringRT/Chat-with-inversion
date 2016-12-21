50620278491b1e57bab81f4b09bafe72ac547120
SpringRT <arinalucky97@gmail.com>
2016-12-21 14:31:36 +0300

    Adding

---
+++ Server/src/ServerGUI.java
@@ -0,0 +1,254 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import java.awt.event.*;
+import java.net.InetAddress;
+
+/*
+ * The server as a GUI
+ */
+
+public class ServerGUI extends JFrame implements ActionListener, WindowListener {
+
+	private static final long serialVersionUID = 1L;
+
+	// the stop and start buttons
+
+	private JButton stopStart;
+
+	// JTextArea for the chat room and the events
+
+	private JTextArea chat, event;
+
+	// The port number
+
+	private JTextField tPortNumber;
+	private JTextField tIp;
+
+	// my server
+
+	private Server server;
+
+	// server constructor that receive the port to listen to for connection as
+	// parameter
+
+	// append message to the two JTextArea
+
+	// position at the end
+
+	void appendRoom(String str) {
+
+		chat.append(str);
+
+		chat.setCaretPosition(chat.getText().length() - 1);
+
+	}
+
+	void appendEvent(String str) {
+
+		event.append(str);
+
+		event.setCaretPosition(chat.getText().length() - 1);
+
+	}
+
+	ServerGUI(int port, String ip) {
+
+		super("Chat Inversion Server");
+
+		server = null;
+
+		// in the NorthPanel the PortNumber the Start and Stop buttons
+
+		JPanel north = new JPanel();
+
+		north.add(new JLabel("Ip address: "));
+
+		tIp = new JTextField(ip);
+
+		north.add(tIp);
+
+		north.add(new JLabel("Port number: "));
+
+		tPortNumber = new JTextField(" " + port);
+
+		north.add(tPortNumber);
+
+		// to stop or start the server, we start with "Start"
+
+		stopStart = new JButton("Start");
+
+		stopStart.addActionListener(this);
+
+		north.add(stopStart);
+
+		add(north, BorderLayout.NORTH);
+
+		// the event and chat room
+
+		JPanel center = new JPanel(new GridLayout(2, 1));
+
+		chat = new JTextArea(80, 80);
+
+		chat.setEditable(false);
+
+		appendRoom("Inversion :)\n");
+
+		center.add(new JScrollPane(chat));
+
+		event = new JTextArea(80, 80);
+
+		event.setEditable(false);
+
+		appendEvent("Events log.\n");
+
+		center.add(new JScrollPane(event));
+
+		add(center);
+
+		// need to be informed when the user click the close button on the frame
+
+		addWindowListener(this);
+
+		setSize(400, 600);
+
+		setVisible(true);
+
+	}
+
+	// start or stop where clicked
+
+	public void actionPerformed(ActionEvent e) {
+
+		// if running we have to stop
+
+		if (server != null) {
+
+			server.stop();
+
+			server = null;
+
+			tPortNumber.setEditable(true);
+
+			stopStart.setText("Start");
+
+			return;
+
+		}
+
+		// OK start the server
+
+		int port;
+		String ip;
+
+		try {
+
+			port = Integer.parseInt(tPortNumber.getText().trim());
+			ip = tIp.getText();
+			InetAddress.getByName(ip);
+
+		}
+
+		catch (Exception er) {
+
+			appendEvent("Invalid port number or ip address\n");
+
+			return;
+
+		}
+
+		// ceate a new Server
+
+		server = new Server(port, ip, this);
+
+		// and start it as a thread
+
+		new ServerRunning().start();
+
+		stopStart.setText("Stop");
+
+		tPortNumber.setEditable(false);
+		tIp.setEditable(false);
+
+	}
+
+	// entry point to start the Server
+
+	public static void main(String[] arg) {
+
+		// start server default port 1620 and ip "127.0.0.1"
+
+		new ServerGUI(1620, "127.0.0.1");
+
+	}
+
+	/*
+	 * 
+	 * If the user click the X button to close the application
+	 * 
+	 * I need to close the connection with the server to free the port
+	 * 
+	 */
+
+	public void windowClosing(WindowEvent e) {
+
+		// if my Server exist
+
+		if (server != null) {
+
+			try {
+
+				server.stop(); // ask the server to close the conection
+			}
+
+			catch (Exception eClose) {
+			}
+			server = null;
+		}
+
+		// dispose the frame
+
+		dispose();
+		System.exit(0);
+
+	}
+
+	// I can ignore the other WindowListener method
+
+	public void windowClosed(WindowEvent e) {
+	}
+
+	public void windowOpened(WindowEvent e) {
+	}
+
+	public void windowIconified(WindowEvent e) {
+	}
+
+	public void windowDeiconified(WindowEvent e) {
+	}
+
+	public void windowActivated(WindowEvent e) {
+	}
+
+	public void windowDeactivated(WindowEvent e) {
+	}
+
+	/*
+	 * 
+	 * A thread to run the Server
+	 * 
+	 */
+
+	class ServerRunning extends Thread {
+		public void run() {
+			server.start(); // should execute until if fails
+			// the server failed
+			stopStart.setText("Start");
+			tPortNumber.setEditable(true);
+			tIp.setEditable(true);
+			appendEvent("Server stopped\n");
+			server = null;
+		}
+	}
+}
